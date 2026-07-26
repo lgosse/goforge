5133dfbeea3ac3aa63f7c44e0870df0be6578d05
Lucas Gossé <lucas@side.co>
2026-07-26 16:50:13 +0200

    fix: logger middleware shape

--- httpmiddlewares/logger.go
+++ httpmiddlewares/logger.go
@@ -7,14 +7,14 @@ import (
 	"github.com/lgosse/goforge"
 )
 
-func LoggerMiddleware(logger *slog.Logger, opts ...middlewareOption) func(http.HandlerFunc) http.HandlerFunc {
+func LoggerMiddleware(logger *slog.Logger, opts ...middlewareOption) func(http.Handler) http.Handler {
 	var options middlewareOptions
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return func(next http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if options.shouldExclude(r) {
 				next.ServeHTTP(w, r)
 				return
@@ -31,6 +31,6 @@ func LoggerMiddleware(logger *slog.Logger, opts ...middlewareOption) func(http.H
 
 			// Call the next handler in the chain
 			next.ServeHTTP(w, r)
-		}
+		})
 	}
 }
